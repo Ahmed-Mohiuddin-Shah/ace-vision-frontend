c588ab80f52c56190d8dc3230cd79b1681324e48
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-08-21 04:06:52 +0500

    chore: moved remote url to .env

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("http://localhost:8000/**")],
+    remotePatterns: process.env.NEXT_PUBLIC_BACKEND_URL
+      ? [new URL(process.env.NEXT_PUBLIC_BACKEND_URL)]
+      : [],
   },
 };
 
