f7efdd35e8bd61a466f66d0b0b705ee2100a73f8
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-08-21 06:47:10 +0500

    chore: modified apiURL constant

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,1 +1,7 @@
-export const apiURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const getAPIURL = () => {
+  const apiURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+  if (!apiURL) {
+    throw new Error("Missing API_URL env var");
+  }
+  return apiURL;
+};
