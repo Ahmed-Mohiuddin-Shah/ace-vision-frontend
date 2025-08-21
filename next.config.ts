06d827ddbc75d39d2d6cfe3b8691c9e87529a488
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-08-21 05:39:39 +0500

    chore: raised limit of upload to 15mb

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
       ? [new URL(process.env.NEXT_PUBLIC_BACKEND_URL)]
       : [],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "15mb", // ⬅️ set this higher (e.g., 10mb, 50mb, 100mb)
+    },
+  },
 };
 
 export default nextConfig;
