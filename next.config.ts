8bb1e0dc27897db73f3f3228e6bcc5857e613d02
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-08-23 22:06:24 +0500

    fix: illconfigured remote pattern url

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: process.env.NEXT_PUBLIC_BACKEND_URL
-      ? [new URL(process.env.NEXT_PUBLIC_BACKEND_URL)]
+      ? [new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/**`)]
       : [],
   },
   experimental: {
